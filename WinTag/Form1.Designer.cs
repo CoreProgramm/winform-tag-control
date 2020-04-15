82bc17bb25f527933c7b94f167ac06950d538233
JT <coreprogramm@gmail.com>
2020-04-15 19:25:48 +0530

    Add project files.

---
+++ WinTag/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WinTag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tagListControl1 = new FerretLib.WinForms.Controls.TagListControl();
+            this.SuspendLayout();
+            // 
+            // tagListControl1
+            // 
+            this.tagListControl1.Location = new System.Drawing.Point(13, 4);
+            this.tagListControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tagListControl1.Name = "tagListControl1";
+            this.tagListControl1.Size = new System.Drawing.Size(1223, 576);
+            this.tagListControl1.TabIndex = 0;
+            this.tagListControl1.Tags = ((System.Collections.Generic.List<string>)(resources.GetObject("tagListControl1.Tags")));
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1249, 594);
+            this.Controls.Add(this.tagListControl1);
+            this.Name = "Form1";
+            this.Text = "Windows Tags";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FerretLib.WinForms.Controls.TagListControl tagListControl1;
+    }
+}
+
---
+++ WinTag/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinTag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Here we are going to add item dynamically when the page is load.
+
+            List<string> mytags = new List<string>();
+            mytags.AddRange(new string[] { "C#", "MVC", "Web API", ".NET Core MVC", "Youtube Videos", "CoreProgramm" });
+            tagListControl1.Tags = mytags;
+        }
+    }
+}
